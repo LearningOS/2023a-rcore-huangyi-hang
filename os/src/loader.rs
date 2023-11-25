6bfeabeda741d895f99de42f9f00d4d2000cfc17
hyh-ubuntu <huangyi.01@qq.com>
2023-11-25 23:29:10 +0800

    more comments

--- os/src/loader.rs
+++ os/src/loader.rs
@@ -69,6 +69,8 @@ pub fn load_apps() {
     }
     let num_app_ptr = _num_app as usize as *const usize;
     let num_app = get_num_app();
+    // first add 1: jump over .quad 13, see link_app.S
+    // second add 1: see comments in link_app.S
     let app_start = unsafe { core::slice::from_raw_parts(num_app_ptr.add(1), num_app + 1) };
     // clear i-cache first
     unsafe {
@@ -82,6 +84,7 @@ pub fn load_apps() {
             .for_each(|addr| unsafe { (addr as *mut u8).write_volatile(0) });
         // load app from data section to memory
         let src = unsafe {
+            // why cast it to u8? treat it as a sequence of bytes.
             core::slice::from_raw_parts(app_start[i] as *const u8, app_start[i + 1] - app_start[i])
         };
         // in ch2, the data arg is APP_BASE_ADDRESS, so apps are loaded into
@@ -92,6 +95,7 @@ pub fn load_apps() {
 }
 
 /// get app info with entry and sp and save `TrapContext` in kernel stack
+/// return trap_ctx_ptr
 pub fn init_app_cx(app_id: usize) -> usize {
     KERNEL_STACK[app_id].push_context(TrapContext::app_init_context(
         get_base_i(app_id),
